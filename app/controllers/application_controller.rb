2e93dcc6a4717fd04e39068f37b5d5de146dfb99
sajeerjaleel <sajeerj@qburst.com>
2014-07-17 14:25:10 +0530

    devise added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exceptionz
+  # protect_from_forgery with: :exceptionz
+
+  private
+
+  # Overwriting the sign_in redirect path method
+  def after_sign_in_path_for(resource) 
+      home_index_path
+  end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root to: "home#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
@@ -14,6 +19,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :home
   # Example resource route with options:
   #   resources :products do
   #     member do
