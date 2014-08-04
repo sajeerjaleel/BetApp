1c55ba7b1b25c3b7de8404fe170a72fb0f06b2cd
qbadmin <qbadmin@qbadmin-desktop.(none)>
2014-08-04 12:43:06 +0530

    cs

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module BetApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w( chart_layout.js )
+    config.assets.initialize_on_precompile = false
   end
 end
