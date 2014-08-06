0ce65b78d29d171534a5cd31e1b35b9e8ede603e
qbadmin <qbadmin@qbadmin-desktop.(none)>
2014-08-06 14:55:30 +0530

    changes saved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -132,7 +132,7 @@ module ApplicationHelper
   def remove_user(user)
   	unless @league.admin_id == user.id 
   		link_to "", remove_user_path(league_id: @league.id,user_id: user.id) ,method: "delete", 
-		  class: "glyphicon glyphicon-trash", :style => "color:red;"
+		  class: "glyphicon glyphicon-trash remove", :style => "color:red;"
 		else
 			"N/A"
 		end
