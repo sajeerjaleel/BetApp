8ba515de5594fe7515fd51640c35219ac7d6d9d3
qbadmin <qbadmin@qbadmin-desktop.(none)>
2014-08-12 17:49:44 +0530

    cs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'bet380.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'sheltered-beach-8162.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
