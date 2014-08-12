b8efb6322cd51fe08b38e81c907c1095e1d4f12f
sajeerjaleel <sajeerj@qburst.com>
2014-08-12 17:21:50 +0530

    password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'bet380.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'mailbet380@gmail.com',
+  :password             => 'bet380password',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end
